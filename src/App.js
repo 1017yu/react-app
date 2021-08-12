029f27dc131c9be7fe957baaf4a395616f07522a
Kled <1017yu@daum.net>
2021-08-12 13:22:51 +0900

    Update react-app

--- src/App.js
+++ src/App.js
@@ -4,10 +4,19 @@ import Content from "./components/Content";
 import Subject from "./components/Subject";
 import './App.css';
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      subject:{title:'WEB', sub:'World Wide Web!'}
+    }
+  }
   render() {  // function render(){}
      return (
       <div className="App"> 
-        <Subject title="React" sub="For UI"></Subject> 
+        <Subject 
+          title={this.state.subject.title} 
+          sub={this.state.subject.sub}>x
+        </Subject> 
         <TOC></TOC>
         <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
       </div>
