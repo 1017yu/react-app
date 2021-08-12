a6053c30452c81263976c9c12d6e4ac7af2c8d61
Kled <1017yu@daum.net>
2021-08-12 14:07:36 +0900

    Update react-app

--- src/App.js
+++ src/App.js
@@ -3,22 +3,37 @@ import TOC from "./components/TOC";
 import Content from "./components/Content";
 import Subject from "./components/Subject";
 import './App.css';
-class App extends Component {
-  constructor(props){
+class App extends Component {  
+  constructor(props){  // 초기화 담당
     super(props);
     this.state = {
-      subject:{title:'WEB', sub:'World Wide Web!'}
+      mode:'welcome',
+      welcome:{title:'Welcome', desc:'Hello React!'},
+      subject:{title:'WEB', sub:'World Wide Web!'},
+      contents:[
+        {id: 0, title:'HTML', desc:"HTML is HyperText Markup Language"},
+        {id: 1, title:'CSS', desc:"CSS is for design"},
+        {id: 2, title:'JavaScript', desc:"JavaScript is for interactive"}
+      ]
     }
   }
-  render() {  // function render(){}
-     return (
+  render() {  // props or state의 값이 변경되면 해당되는 component의 render 함수가 호출된다. 즉 화면이 다시 그려짐
+    var _title, _desc = null;
+    if(this.state.mode === 'welcome') {
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if(this.state.mode === 'read') {
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+    }
+    return (
       <div className="App"> 
         <Subject 
           title={this.state.subject.title} 
           sub={this.state.subject.sub}>x
         </Subject> 
-        <TOC></TOC>
-        <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
+        <TOC data={this.state.contents}></TOC>
+        <Content title={_title} desc={_desc}></Content>
       </div>
     );
   }
--- src/components/TOC.js
+++ src/components/TOC.js
@@ -1,13 +1,18 @@
-import React, { Component } from 'react';  // react 라는 library에서 Component라는 class를 loading함.
+import React, { Component } from 'react';  // React 라는 library에서 Component라는 class를 loading함.
 
 class TOC extends Component {
   render() {
+    var lists = [];
+    var data = this.props.data;
+    var i = 0;
+    while(i < data.length) {
+      lists.push(<li key={data[i].id}><a href={"/content/"+data[i].id}>{data[i].title}</a></li>);
+      i++;
+    }
     return(
       <nav>
         <ul>
-          <li><a href="1.html">HTML</a></li>
-          <li><a href="2.html">CSS</a></li>
-          <li><a href="3.html">JavaScript</a></li>
+          {lists}
         </ul> 
       </nav> 
     );
