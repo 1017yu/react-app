3ce3cfb6ee7162c78a2c2a191eb82f5d85d3948d
Kled <1017yu@daum.net>
2021-08-24 16:50:31 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import TOC from "./components/TOC";
 import ReadContent from "./components/ReadContent";
 import CreateContent from "./components/CreateContent";
+import UpdateContent from "./components/UpdateContent";
 import Subject from "./components/Subject";
 import Control from "./components/Control";
 import './App.css';
@@ -21,38 +22,72 @@ class App extends Component {
       ]
     }
   }
-  render() {  // props or state의 값이 변경되면 해당되는 component의 render 함수가 호출된다. 즉 화면이 다시 그려짐
+
+  getReadContent(){
+    var i = 0;
+      while(i < this.state.contents.length){
+        var data = this.state.contents[i];
+        if(data.id === this.state.selected_content_id) {
+          return data;
+        }
+        i++;
+      }
+  }
+
+  getContent(){
+    console.log('App render');
     var _title, _desc, _article = null;
     
     if(this.state.mode === 'welcome') {
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc; 
       _article = <ReadContent title={_title} desc={_desc}></ReadContent>
-    } else if(this.state.mode === 'read') {
-      var i = 0;
-      while(i < this.state.contents.length){
-        var data = this.state.contents[i];
-        if(data.id === this.state.selected_content_id) {
-          _title = data.title;
-          _desc = data.desc;
-          break;
-        }
-        i++;
-      }
-      _article = <ReadContent title={_title} desc={_desc}></ReadContent>
-    } else if(this.state.mode === 'create'){
+    } 
+    else if(this.state.mode === 'read') {
+      var _content= this.getReadContent();
+      _article = <ReadContent title={_content.title} desc={_content.desc}></ReadContent>
+    } 
+    else if(this.state.mode === 'create'){
       _article = <CreateContent onSubmit={function(_title, _desc){
         // add content to this.state.contents
         this.max_content_id++;
-        this.state.contents.push(
+        /* this.state.contents.push(
           {id: this.max_content_id, title: _title, desc: _desc}
-        );
+        ); 
+        push는 기존의 contents까지 변화를 주므로 concat 메소드 사용
+        */
+        var _contents = this.state.contents.concat(
+          {id: this.max_content_id, title: _title, desc: _desc}
+        )
         this.setState({
-          contents: this.state.contents
+          contents: _contents
         });
         console.log(_title, _desc);
       }.bind(this)}></CreateContent>
+    } 
+    else if(this.state.mode === 'update'){
+      _content = this.getReadContent();
+      _article = <UpdateContent data={_content} onSubmit={function(_title, _desc){
+        // add content to this.state.contents
+        this.max_content_id++;
+        /* this.state.contents.push(
+          {id: this.max_content_id, title: _title, desc: _desc}
+        ); 
+        push는 기존의 contents까지 변화를 주므로 concat 메소드 사용
+        */
+        var _contents = this.state.contents.concat(
+          {id: this.max_content_id, title: _title, desc: _desc}
+        )
+        this.setState({
+          contents: _contents
+        });
+        console.log(_title, _desc);
+      }.bind(this)}></UpdateContent>
     }
+    return _article;
+  }
+
+  render() {  // props or state의 값이 변경되면 해당되는 component의 render 함수가 호출된다. 즉 화면이 다시 그려짐
     return (
       <div className="App"> 
         <Subject 
@@ -78,7 +113,7 @@ class App extends Component {
             mode:_mode
           })
         }.bind(this)}></Control>
-        {_article}
+        {this.getContent()}
       </div>
     );
   }
